3b57f8a13ff8cd45b55356c05dd851d801f31c59
donal <donalspring@gmail.com>
2019-10-21 18:32:44 +0100

    fix health check

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "OK", "true" };
+        }
+    }
+}
